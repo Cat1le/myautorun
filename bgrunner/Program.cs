5740ef2875e2c6ac87dd17d7f0f9b678688c1058
Noki3138 <nikitaserov912@gmail.com>
2023-05-13 23:11:19 +0300

    added bgrunner

---
+++ bgrunner/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Starting: " + String.Join(" ", args));
+String arguments = String.Join(" ", args.Skip(1).ToArray());
+String command = args[0];
+
+var p = new Process();
+p.StartInfo = new ProcessStartInfo(command);
+p.StartInfo.Arguments = arguments;
+p.StartInfo.WorkingDirectory = Path.GetDirectoryName(command);
+p.StartInfo.CreateNoWindow = true;
+p.StartInfo.UseShellExecute = false;
+p.Start();
